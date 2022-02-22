37485a5b57d2de067fc4d4a9f6eb04d187c52db9
ganeshgaikwad56 <gaikwadganesh070297@gmail.com>
2022-02-22 23:04:49 +0530

    Adding Snake And Ladder Start position program file

---
+++ SnakeAndLadder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    /// <summary>
+    /// UC_1_SnakeAndLadder
+    /// </summary>
+    public class SnakeAndLadder
+    {
+        public static void StartPositon()
+        {
+            int position = 0;
+            Console.WriteLine("Current Position of a player :" + position);
+
+            while (position <= 100)
+            {
+                Console.WriteLine("Reaching Position of a player = " +position);
+                position++;
+            }
+        }
+    }
+}
