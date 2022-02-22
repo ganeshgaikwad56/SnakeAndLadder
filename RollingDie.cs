0cdc1006d99e8ef8b44066851e4e2071e14657f4
ganeshgaikwad56 <gaikwadganesh070297@gmail.com>
2022-02-22 23:11:18 +0530

    Adding Rolling Dies program file

---
+++ RollingDie.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    /// <summary>
+    /// UC_2_Finding Number
+    /// </summary>
+    public class RollingDie
+
+    {
+        public static void RollsTheDie()
+        {
+           
+
+            Random random = new Random();
+            int DiceRoll = random.Next(1,7);
+
+           Console.WriteLine("The Number is :" + DiceRoll);
+            
+        }
+    }
+}
