da8c57060ea3ace7d9fadd7e5e4d1d7870ebcd65
ganeshgaikwad56 <gaikwadganesh070297@gmail.com>
2022-02-22 22:51:45 +0530

    Pushing Greeting message for Snake&Ladder program

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    public class SnakeAndLadderService
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Wellcome to Snake & Ladder Program");
+            //SnakeAndLadder.StartPositon();
+            // RollingDie.RollsTheDie();
+            // WiningPosition.PlaySnakeAndLadder();
+            //CheckPlayer.CheckThePlayer();
+           // ExactWinningPosition.PlayGame();
+           TwoPlayer.SwitchPlayers();
+        }
+    }
+}
